b8ff256ebd100d38be82f0f50086a41f3f761881
Jhonatan S. Becerra <jsbpdeveloper@gmail.com>
2023-01-12 11:00:52 -0500

    Fixed cors errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors')
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
@@ -7,6 +8,21 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whiteList = [
+  'http://localhost:8080',
+  'https://myapp.co'
+]
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido'))
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', function (req, res) {
   res.send('Home Page');
